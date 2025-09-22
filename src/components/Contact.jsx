34cff1292755197617f9f12e83cf83d5364fe5c2
Kamesh-Kadimisetty <323103311022@gvpce.ac.in>
2025-09-22 23:19:36 +0530

    refactor: integrate framer-motion for animations in Codingprofiles, Contact, and Experience components

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,13 +7,13 @@ const Contact = () => {
           whileInView={{ opacity: 1, y: 0 }}
           initial={{ opacity: 0, y: -100 }}
           transition={{ duration: 0.5 }}
-        className="my-10 text-4xl text-center">Let's Connect🤝</motion.h1>
+        className="my-10 text-4xl text-center">Let&apos;s Connect🤝</motion.h1>
         <div className="tracking-tighter text-center">
             <motion.p
               whileInView={{ opacity: 1, x: 0 }}
               initial={{ opacity: 0, x: -100 }}
               transition={{ duration: 1 }}
-            className="my-4">I'd love to hear from you! Feel free to reach out via email.</motion.p>
+            className="my-4">I&apos;d love to hear from you! Feel free to reach out via email.</motion.p>
             <a href="mailto:kameshkadimisetty@gmail.com" className="text-blue-500 no-underline hover:underline">
   kameshkadimisetty@gmail.com
 </a>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,14 @@
 import { EXPERIENCES } from "../constants";
+import { motion } from "framer-motion";
 
 const Experience = () => {
   return (
-    <div className="pb-4 border-b border-neutral-900">
-      <h1 className="my-20 text-4xl text-center">Experience</h1>
+    <div id="experience" className="pb-4 border-b border-neutral-900">
+      <motion.h1 
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-4xl text-center">Experience</motion.h1>
       <div>
         {EXPERIENCES.map((experience, index) => (
           <div
@@ -11,40 +16,48 @@ const Experience = () => {
             className="flex flex-wrap mb-8 lg:justify-center"
           >
             {/* Left side - Year */}
-            <div className="w-full lg:w-1/4">
+            <motion.div 
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: -100 }}
+              transition={{ duration: 1.0 }}
+              className="w-full lg:w-1/4">
               <p className="mb-2 text-sm text-neutral-400">
                 {experience.year}
               </p>
-            </div>
+            </motion.div>
 
             {/* Right side - Role, Company, Project, Description, Tech */}
-            <div className="w-full max-w-xl lg:w-3/4">
-            <h6 className="mb-1 text-xl font-bold">{experience.role}</h6>
-            <p className="mb-2 text-sm font-medium text-purple-300">
-                {experience.company}
-              </p>
-
-              {/* Project Name */}
-              {experience.project && (
-                <p className="mb-2 text-sm font-medium text-purple-200">
-                  Project: {experience.project}
+            <motion.div 
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: 100 }}
+              transition={{ duration: 1.0 }}
+              className="w-full max-w-xl lg:w-3/4">
+              <h6 className="mb-1 text-xl font-bold">{experience.role}</h6>
+              <p className="mb-2 text-sm font-medium text-purple-300">
+                  {experience.company}
                 </p>
-              )}
 
-              <p className="mb-4 text-neutral-400">
-                {experience.description}
-              </p>
+                {/* Project Name */}
+                {experience.project && (
+                  <p className="mb-2 text-sm font-medium text-purple-200">
+                    Project: {experience.project}
+                  </p>
+                )}
 
-              {/* Technologies */}
-              {experience.technologies.map((tech, index) => (
-                <span
-                  key={index}
-                  className="inline-block px-2 py-1 mt-4 mr-2 text-sm font-medium text-purple-500 rounded bg-neutral-900"
-                >
-                  {tech}
-                </span>
-              ))}
-            </div>
+                <p className="mb-4 text-neutral-400">
+                  {experience.description}
+                </p>
+
+                {/* Technologies */}
+                {experience.technologies.map((tech, index) => (
+                  <span
+                    key={index}
+                    className="inline-block px-2 py-1 mt-4 mr-2 text-sm font-medium text-purple-500 rounded bg-neutral-900"
+                  >
+                    {tech}
+                  </span>
+                ))}
+            </motion.div>
           </div>
         ))}
       </div>
